289480daf4341341ab1866ae05f8bbe858a17813
Nima Mohseni <Nima Mohseni>
2017-01-15 16:20:29 -0800

    First commit

---
+++ Arduino_Code/Serial_Send/Serial_Send.ino
@@ -0,0 +1,18 @@
+ #include <SoftwareSerial.h>
+ SoftwareSerial hc12(5,6);
+ void setup()  
+ {  
+  Serial.begin(9600); 
+  hc12.begin(9600);
+  //Serial.print(0); 
+ }  
+ void loop()  
+ {  
+ // if(Serial.available())  
+//  {  
+ //  char c = Serial.read();  
+   //Serial.print(c);  
+  //}  
+   if(Serial.available()) hc12.write(Serial.read());
+  if(hc12.available()) Serial.write(hc12.read());
+ }  
